0bb68e82cc6da9a81a1d38c6868eb0f377c36b84
UriShilo <urishilo360@gmail.com>
2023-11-15 20:48:50 +0200

    make  ball grow a random size on click

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 function onBallClick() {
   const elBall = document.querySelector('.ball')
-  elBall.innerText = +elBall.innerText + 50
+  elBall.innerText = +elBall.innerText + getRandomInt(20,61)
   if (+elBall.innerText > 400) elBall.innerText = 100
   elBall.style.width = elBall.innerText + 'px'
   elBall.style.height = elBall.innerText + 'px'
