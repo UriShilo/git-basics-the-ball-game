3a6707594a7a269e203971a44829649ebdb57735
UriShilo <urishilo360@gmail.com>
2023-11-15 22:02:19 +0200

    add maxDiameter parameter to onBallClick

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 'use strict'
 
-function onBallClick() {
+function onBallClick(maxDiameter) {
   const elBall = document.querySelector('.ball')
   elBall.innerText = +elBall.innerText + getRandomInt(20,61)
-  if (+elBall.innerText > 400) elBall.innerText = 100
+  if (+elBall.innerText > maxDiameter) elBall.innerText = 100
   elBall.style.width = elBall.innerText + 'px'
   elBall.style.height = elBall.innerText + 'px'
   elBall.style.backgroundColor = getRandomColor()
